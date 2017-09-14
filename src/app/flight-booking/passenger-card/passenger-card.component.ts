98e3dc848f248944432908d9dc26ee41c7afd351
ManfredSteyer <manfred.steyer@gmx.net>
2017-09-14 16:09:12 +0200

    life cycle hooks

--- src/app/flight-booking/passenger-card/passenger-card.component.ts
+++ src/app/flight-booking/passenger-card/passenger-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Passenger } from '../../entities/passenger';
 
 @Component({
@@ -6,16 +6,38 @@ import { Passenger } from '../../entities/passenger';
   templateUrl: './passenger-card.component.html',
   styleUrls: ['./passenger-card.component.css']
 })
-export class PassengerCardComponent implements OnInit {
+export class PassengerCardComponent implements OnInit, OnDestroy, OnChanges {
 
-  constructor() { }
+  constructor() {
+    console.debug('ctor', this.selected, this.item)
+  }
 
   @Input() item: Passenger;
   @Input() selected: boolean;
   @Output() selectedChange = new EventEmitter<boolean>();
 
   ngOnInit() {
+    console.debug('init', this.selected, this.item);
+    // Would introduce cycle --> is not allowed
+    // this.selectedChange.next(true);
   }
+
+  ngOnDestroy(): void {
+    console.debug('destroy', this.selected, this.item)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    console.debug('changes', this.selected, this.item)
+    if (changes['selected']) {
+      console.debug('selected changed');
+    }
+    if (changes['item']) {
+      console.debug('item changed');
+    }
+
+  }
+
 
   select() {
     this.selected = true;
